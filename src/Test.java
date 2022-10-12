8d6c712d79ee27875cc7ed6b11a437b7fc9cf810
Tri-Nguyen-C0822g1 <tringuyen10396@gmail.com>
2022-10-12 11:45:57 +0700

    add project

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Phu");
+        student.setClasses("CO8");
+        System.out.println(student.getClasses());
+    }
+}
